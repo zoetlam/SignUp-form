459552536988085fafe5223f8319fde2c3323cfe
Thi Lam <118875553+zoetlam@users.noreply.github.com>
2023-09-24 20:58:14 +0700

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,18 @@
 let emailError = document.querySelector('#email-error');
 let emailField = document.querySelector('#email-field');
+let inputValue = "";
+emailField.addEventListener("input", function(event) {
+    inputValue = event.target.value;
+});
 
 function emailCheck(){
     if (!emailField.value.match((/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))){
-        emailError.innerHTML = "Valid email required"
-        return false;
+        emailError.innerHTML = "Valid email required";
     } else {
         emailError.innerHTML = "";
         document.querySelector('#sign-up').classList.add('hidden');
         document.querySelector('#success-popup').classList.remove('hidden');
-        return true;
+        document.querySelector('span').innerHTML = inputValue;
     }
 }
 
