f94d089d422ded19b57aa2852728990b27411606
Zoe T. Lam <118875553+zoetlam@users.noreply.github.com>
2024-02-24 14:20:31 +0700

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,25 @@
-let emailError = document.querySelector('#email-error');
-let emailField = document.querySelector('#email-field');
-let inputValue = "";
-emailField.addEventListener("input", function(event) {
-    inputValue = event.target.value;
+let emailError = document.querySelector("#email-error");
+let emailField = document.querySelector("#email-field");
+let form = document.querySelector("form");
+
+// Prevent form submit so that page does not refresh
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 });
 
-function emailCheck(){
-    if (!emailField.value.match((/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))){
-        emailError.innerHTML = "Valid email required";
-    } else {
-        emailError.innerHTML = "";
-        document.querySelector('#sign-up').classList.add('hidden');
-        document.querySelector('#success-popup').classList.remove('hidden');
-        document.querySelector('span').innerHTML = inputValue;
-    }
+function emailCheck() {
+  if (
+    !emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
+  ) {
+    emailError.innerHTML = "Valid email required";
+  } else {
+    emailError.innerHTML = "";
+    document.querySelector("#sign-up").classList.add("hidden");
+    document.querySelector("#success-popup").classList.remove("hidden");
+    document.querySelector("span").innerHTML = emailField.value;
+  }
 }
 
-document.querySelector('#dismiss').addEventListener('click', function(){
-    document.querySelector('#sign-up').classList.remove('hidden');
-    document.querySelector('#success-popup').classList.add('hidden');
-})
+function reload() {
+  window.location.reload();
+}
